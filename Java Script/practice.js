9ab0536c44a073c3e5eb1496bc194e5984d1c48b
aasif-codes <muhammed.aasif@outlook.com>
2026-02-21 08:00:41 +0530

    Add files via upload

---
+++ Java Script/practice.js
@@ -0,0 +1,31 @@
+let name="Tom", student=name;
+console.log(student);
+
+
+
+
+
+ /* 
+    # Js Variable naming Rules
+    
+ 1. Variable name must be unique (cannot use same name in same scope)
+
+
+ 2. Numbers are allowed, but the name should NOT start with a number
+ Correct: name1 Wrong: 1name
+
+
+ 3. Only "_" (underscore) and "$" (dollar sign) are allowed as special characters
+ Other special characters like @, #, -, % are NOT allowed
+
+ 4. No spaces are allowed between letters or words
+ Correct: userName
+ Wrong: user name
+
+
+ 5. JavaScript is case-sensitive
+ age, Age, and AGE are different variables
+
+
+ 6. Variable value can be changed if declared with "let"
+ If declared with "const", the value cannot be changed */
